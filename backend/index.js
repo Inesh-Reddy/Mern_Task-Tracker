eb4d482d0c324908a3a751095de0e2fb8639676a
Inesh-Reddy <ineshreddy007@gmail.com>
2025-05-07 10:39:52 +0530

    Task Tracker : Initialized frontend

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
-
+require('dotenv').config()
 const app = express();
-
+const PORT = process.env.PORT;
 app.use(express.json());
 
 app.get("/",(req,res) => {
     res.send(`Task Tracker App`);
 })
 
-app.listen(3000, ()=>{
+app.listen(PORT, ()=>{
     console.log(`Server is listening on port : 3000`);
 })
