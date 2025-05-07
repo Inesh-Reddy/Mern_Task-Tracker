b9f01bff573f242c0845cd9b6da1008869c181bb
Inesh-Reddy <ineshreddy007@gmail.com>
2025-05-07 13:57:02 +0530

    Task Tracker : Implemented Task Model and tested

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const { connectDB } = require("./db");
+const { mongo, default: mongoose } = require("mongoose");
+const Task = require("./models/task");
 
 dotenv.config();
 const app = express();
@@ -17,6 +19,21 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.json({
     message: `Task Tracker API`,
+  });
+});
+
+app.post("/tasks", async (req, res) => {
+  const title = req.body.title;
+  const description = req.body.description;
+
+  const task = new Task({
+    title: title,
+    description: description,
+  });
+  await task.save();
+
+  res.json({
+    message: `Created`,
   });
 });
 
