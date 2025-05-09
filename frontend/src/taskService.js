f03d7b3f681e1ce5b573868eeb3d652debc8426e
Inesh-Reddy <ineshreddy007@gmail.com>
2025-05-09 17:45:09 +0530

    Task Tracker : Refactored Updating methods and added updateUsingCheckBox method to handle all cases

--- frontend/src/taskService.js
+++ frontend/src/taskService.js
@@ -27,15 +27,31 @@ export const adding = async (title, description) => {
   }
 };
 
-export const updating = async (id, completed, title, description) => {
+export const updateUsingCheckBox = async (id, completed) => {
+  try {
+    console.log(completed);
+    const response = await axios({
+      method: "put",
+      url: `http://localhost:3000/api/tasks/${id}`,
+      data: {
+        completed: completed,
+      },
+    });
+    return response;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
+export const updating = async (id, title, description) => {
   try {
     const response = await axios({
       method: "put",
       url: `http://localhost:3000/api/tasks/${id}`,
       data: {
-        ...(title !== undefined && { title }),
-        ...(description !== undefined && { description }),
-        ...(completed !== undefined && { completed }),
+        title: title,
+        description: description,
       },
     });
     return response;
